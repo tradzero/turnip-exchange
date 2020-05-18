7514ec06afc7736fb233c5e8f8f09ff70c4f646a
tradzero <admin@drakframe.com>
2020-05-18 20:45:26 +0800

    sql debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,27 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        Log::info('============ URL: '.request()->fullUrl().' ===============');
+        DB::listen(function (QueryExecuted $query) {
+            Log::info($query->time);
+            $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+            $bindings = $query->connection->prepareBindings($query->bindings);
+            $pdo = $query->connection->getPdo();
+            Log::info(vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings)));
+        });
     }
 }
